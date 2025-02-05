d426908e74d860d421e07466025a1a2a647dd84c
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2025-02-05 09:42:49 +0100

    feat(TUI): implement managing habits
    managing habits include: adding and removing completion from habit

--- src/HabitTracker/TUI.cs
+++ src/HabitTracker/TUI.cs
@@ -40,6 +40,7 @@ namespace HabitTracker
                 Console.ForegroundColor = ConsoleColor.Blue;
                 Console.WriteLine("1. Add new habit");
                 Console.WriteLine("2. Remove a habit");
+                Console.WriteLine("3. Manage Habit");
                 Console.WriteLine("9. Quit Program");
 
                 string? answer = Console.ReadLine();
@@ -53,6 +54,9 @@ namespace HabitTracker
                             break;
                         case 2:
                             RemoveHabit();
+                            break;
+                        case 3:
+                            ManageHabit();
                             break;
                         case 9:
                             return;
@@ -108,5 +112,86 @@ namespace HabitTracker
             }
         }
 
+        /// <summary>
+        /// Add or remove completion from habit.
+        /// </summary>
+        private void ManageHabit()
+        {
+            Console.WriteLine("Please choose a habit: ");
+            var i = 0;
+            foreach (var h in Manager.Habits)
+            {
+                i += 1;
+                Console.WriteLine($"{i}: {h.Name}");
+            }
+
+            int index;
+            bool success = Int32.TryParse(Console.ReadLine(), out index);
+
+            if (!success)
+            {
+                Console.WriteLine("You must choose a number.");
+                return;
+            }
+
+            index -= 1; // Arrays are 0-indexed, but we present them as 1-indexed
+
+            Console.Clear();
+            var habit = Manager.Habits[index];
+            habit.PrintCompletionDates();
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("1. Add Completion");
+            Console.WriteLine("2. Remove Completion Date");
+
+            success = Int32.TryParse(Console.ReadLine(), out index);
+
+            switch (index)
+            {
+                case 1:
+                    habit.AddCompletion(DateTime.Now);
+                    break;
+                case 2:
+                    RemoveCompletion(habit);
+                    break;
+                default:
+                    Console.WriteLine("You must choose a number.");
+                    return;
+            }
+        }
+
+        /// <summary>
+        /// Gives a guide to remove a completion.
+        /// </summary>
+        /// <param name="habit">The habit which a completion is to be removed from.</param>
+        private void RemoveCompletion(Habit habit)
+        {
+            Console.WriteLine($"Habit: {habit.Name}. Please choose a completion you want to delete: ");
+
+            var i = 0;
+            foreach (var completion in habit.Completions)
+            {
+                i += 1;
+                Console.WriteLine($"{i}: {completion}");
+            }
+
+
+            Console.Write("Number: ");
+
+            int index;
+            bool success = Int32.TryParse(Console.ReadLine(), out index);
+
+            if (!success)
+            {
+                Console.WriteLine("You must write a valid number.");
+                return;
+            }
+
+            habit.RemoveCompletion(habit.Completions[index]);
+
+            Console.WriteLine($"Completion {habit.Completions} deleted.");
+            Console.WriteLine($"Press any key to continue");
+
+            Console.ReadKey();
+        }
     }
 }
