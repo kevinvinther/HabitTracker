c2ef311ba744322ae71fd150d12cb5564622e508
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2025-03-05 11:12:53 +0100

    chore: add script for code coverage gen

---
+++ tests/HabitTracker.Tests/create_coverage.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+dotnet test --collect:"XPlat Code Coverage" --results-directory coverage/
+
+reportgenerator \
+    -reports:"./coverage/**/coverage.cobertura.xml" \
+    -targetdir:"coveragereport" \
+    -reporttypes:Html
