5e8f4903ed09706f68df5f383e7ba87106c340c5
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2025-03-16 17:08:40 +0100

    refactor: make DateTimeHelper a static class
    Also adds ParseDateStrings to DateTimeHelper

--- src/HabitTracker/Helpers/DateTimeHelper.cs
+++ src/HabitTracker/Helpers/DateTimeHelper.cs
@@ -1,6 +1,8 @@
 using System.Globalization;
 
-public class DateTimeHelper
+namespace HabitTracker;
+
+public static class DateTimeHelper
 {
     public const string DateFormat = "yyyy-MM-dd HH:mm:ss";
 
@@ -9,4 +11,16 @@ public class DateTimeHelper
 
     public static DateTime Parse(String dt) =>
         DateTime.ParseExact(dt, DateFormat, CultureInfo.InvariantCulture);
+
+    public static List<DateTime> ParseDateStrings(IEnumerable<string> date_list)
+    {
+        List<DateTime> dts = new List<DateTime>();
+
+        foreach (var date in date_list)
+        {
+            dts.Add(DateTimeHelper.Parse(date));
+        }
+
+        return dts;
+    }
 }
